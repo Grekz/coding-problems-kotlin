671d067a4ebcb2f76d47d1972f81ec6781b81179
Grekz <vidbla.grekz@gmail.com>
2019-03-15 20:54:26 +0100

    KLCE-319. Bulb Switcher

---
+++ src/mx/grekz/leetcode/medium/E319_BulbSwitcher.kt
@@ -0,0 +1,10 @@
+package mx.grekz.leetcode.medium
+
+/**
+ * @author grekz
+ */
+class E319_BulbSwitcher {
+    fun bulbSwitch(n: Int): Int {
+        return Math.sqrt(n.toDouble()).toInt()
+    }
+}
